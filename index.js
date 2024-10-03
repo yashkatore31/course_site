7f22c65777364b4e438299a423df6c1e98031781
yashkatore31 <ybkatore31@gmail.com>
2024-10-03 14:47:44 +0530

    modified the index.js

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
 const express = require("express");
+const { userRouter} = require("./routes/user");
+const { courseRouter} = require("./routes/course");
 
 const app = express();
+
 app.use(express.json());
 
-app.use("user",userRouter);
-app.post("user/signup/" ,function(req, res){
-
-    const email = req.body.email;
-    const password = req.body.password;
-
-
-
-});
-
-app.post("user/signin" ,function(req, res){
-
-});
-
-app.post("user/purchase" ,function(req, res){
-
-});
-
-app.get("course/purchase" ,function(req, res){
-
-});
-
-app.get("/courses" ,function(req, res){
-
-});
-
+app.use("/user",userRouter);
+app.use("course",courseRouter);
 
 app.listen(3000);
